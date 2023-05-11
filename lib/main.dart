4a1d9083b5db1405c57cc1f17f51305945c0d177
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-11 16:55:56 +0530

    Flutter Device Preview

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'basic.dart';
+import 'custom_plugin.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      tools: const [
+        ...DevicePreview.defaultTools,
+        CustomPlugin(),
+      ],
+      builder: (context) => const BasicApp(),
+    ),
+  );
+}
